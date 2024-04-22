db3626ecbcbf9fac59cf918d4f1fa63c461b26b0
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-04-22 19:38:07 -0300

    layout ok

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btns = document.querySelectorAll('button');
 const result = document.querySelector('.result');
 const partial = document.querySelector('.partial');
+const body = document.querySelector('.body');
 
-//forEach nos botões para adicionar um evento de click que chama a função de mesmo nome
 btns.forEach((button, value) => {
     button.addEventListener('click', () => {
         click(value);
@@ -13,10 +13,29 @@ btns.forEach((button, value) => {
 function click( data ) {
     btns.forEach((button, value) => {
         if(data === value) {
-            if (value === 'dell') clear();
-            else if (value === 'close') 
+            if (isNaN(btns[value].value)) {
+                switch (btns[value].value) {
+                    case 'dell':
+                        clear();
+                        break;
+                    case 'close':
+                        eraser();
+                        break;
+                    case 'equal':
+                        calculate();
+                        break;
+                    case 'invert':
+                        invertCalc();
+                }
+            } else {
+                show(btns[value].value);
+            }
         }
     });
+}
+
+function invertCalc() {
+    body.classList.toggle('cientific');
 }
 
 //função para mostrar valor digitado na tela
@@ -33,16 +52,16 @@ function clear() {
 
 //função que apaga ultimo valor digitado
 function eraser() {
-    resul.innerHTML = resul.innerHTML.slice(0, -1);
+    result.innerHTML = result.innerHTML.slice(0, -1);
     upDisplayParse();
-    if (resul.innerHTML.length < 2) {
+    if (result.innerHTML.length < 2) {
         clear();
     }
 }
 
 //função que faz o calculo da espressão e mostra como resultado parcial
 function upDisplayParse() {
-    const expression = resul.innerHTML;
+    const expression = result.innerHTML;
     if (expression) {
         let result = eval(expression);
         parse.innerHTML = result;
@@ -51,6 +70,6 @@ function upDisplayParse() {
 
 //função que transforma o resultado parcial em resultado final
 function calculate() {
-    resul.innerHTML = parse.innerHTML;
+    result.innerHTML = parse.innerHTML;
     parse.innerHTML = '';
 }
