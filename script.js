8e5a82f12f00c87f884ce7980bf3dbffb37850bf
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-04-23 14:45:45 +0000

    ...

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ const result = document.querySelector('.result');
 const partial = document.querySelector('.partial');
 const body = document.querySelector('.body');
 
+body.classList.add('cientific');
+
+result.innerHTML = '0';
+
+let lastTerm = 0;
+let fistTerm = '';
+let terms = [];
+let operators = [];
+let numTemp = '';
+let isOperator = false;
+
 btns.forEach((button, value) => {
     button.addEventListener('click', () => {
         click(value);
@@ -15,6 +26,9 @@ function click( data ) {
         if(data === value) {
             if (isNaN(btns[value].value)) {
                 switch (btns[value].value) {
+                    case '.':
+                        show(',');
+                        break;
                     case 'dell':
                         clear();
                         break;
@@ -26,6 +40,16 @@ function click( data ) {
                         break;
                     case 'invert':
                         invertCalc();
+                        break;
+                    case 'porcent':
+                        porcent(lastTerm, fistTerm);
+                        break;
+                    case '+':
+                    case '-':
+                    case '/':
+                    case '*':
+                        show(btns[value].value);
+                        break;
                 }
             } else {
                 show(btns[value].value);
@@ -38,15 +62,65 @@ function invertCalc() {
     body.classList.toggle('cientific');
 }
 
+function operator(op) {
+    terms.push(numTemp);
+    operators.push(op);
+    numTemp = '';
+}
+
+function porcent (value, term) {
+    if (value) {
+        let out = value * (term/100);
+        show(out);
+    } else {
+        console.log('por: ', term);
+
+        let out = term / 100;
+
+        console.log('saida: ', out);
+
+        clear();
+        show(out);
+    }
+}
+
 //função para mostrar valor digitado na tela
 function show(value) {
-    result.innerHTML += value;
+    if (isOperator) {
+        if (value === ',') return;
+        else if (['+', '-', '/', "*"].includes(value)) return;
+        else {
+            result.innerHTML += value;
+            numTemp += value;
+            isOperator = false;
+        }
+    } else {
+        if (value === ',') {
+            numTemp += '.';
+            result.innerHTML += value;
+        } else if (['+', '-', '*', '/'].includes(value)) {
+            let symbol;
+            if (value === '*') symbol = 'x';
+            else symbol = value;
+            result.innerHTML += symbol;
+            operator(value);
+            isOperator = true;
+        } else {
+            if (result.innerHTML === '0'){
+                result.innerHTML = value;
+                numTemp = value;
+            } else {
+                result.innerHTML += value;
+                numTemp += value;
+            }
+        }
+    }
     upDisplayParse();
 }
 
 //função acionada ao clicar no botão "C" que limpa o display
 function clear() {
-    result.innerHTML = '';
+    result.innerHTML = '0';
     partial.innerHTML = '';
 }
 
@@ -61,15 +135,11 @@ function eraser() {
 
 //função que faz o calculo da espressão e mostra como resultado parcial
 function upDisplayParse() {
-    const expression = result.innerHTML;
-    if (expression) {
-        let result = eval(expression);
-        parse.innerHTML = result;
-    }
+    
 }
 
 //função que transforma o resultado parcial em resultado final
 function calculate() {
-    result.innerHTML = parse.innerHTML;
-    parse.innerHTML = '';
+    result.innerHTML = partial.innerHTML;
+    partial.innerHTML = '';
 }
