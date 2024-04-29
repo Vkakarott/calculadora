6a3857228ca5dcfc7355040a465a70363dbf2a47
Vitor Gomes <133702403+Vkakarott@users.noreply.github.com>
2024-04-28 23:11:32 -0300

    ...pt1

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
+const generate  = document.querySelector('html');
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result');
 const partial = document.querySelector('.partial');
 const body = document.querySelector('.body');
-
-body.classList.add('cientific');
-
-function invertCalc() {
-    body.classList.toggle('cientific');
-}
-
-let expression = '';
-let numTemp = '';
-let isOperator = false;
-let calcPartial = '';
+const trigger = document.querySelectorAll('.sec');
+const deg = document.querySelector('#deg');
 
 buttons.forEach((button, value) => {
     button.addEventListener('click', () => {
@@ -20,7 +12,7 @@ buttons.forEach((button, value) => {
     });
 });
 
-function click( data ) {
+function click(data) {
     const button = buttons[data];
 
     if (!button) return;
@@ -40,7 +32,14 @@ function click( data ) {
         case 'invert':
             invertCalc();
             break;
+        case 'mode':
+            themerMode();
+            break;
         case 'porcent':
+            porcent();
+            break;
+        case 'second':
+            invertFunction();
             break;
         default:
             show(buttonValue);
@@ -48,67 +47,116 @@ function click( data ) {
     upDisplayParse();
 }
 
+function invertCalc() {
+    body.classList.toggle('cientific');
+}
+
+function themerMode() {
+    generate.classList.toggle('light');
+}
+
+let isOperator = false;
+let expression = '';
+let numTemp = '';
+let calcPartial = '';
+let residue = 0;
+let hasComma = false;
+let invertTrigger = false;
+
 function show(value) {
     if (result.innerHTML.length > 9) return;
-    else if (isOperator && !isNaN(value)) {
-        result.innerHTML += value;
+
+    if (!isNaN(value)) {
+        value = String(value);
+        value = value.replace(/\./g, ',');
+        if (result.innerHTML === '0') result.innerHTML = value;
+        else result.innerHTML += value;
+
         numTemp += value;
-        isOperator = false;
         calcPartial = expression + numTemp;
-    } else {
-        if (value === ',') {
-            if (result.innerHTML === '0') numTemp = '0.';
-            else numTemp += '.';
-            result.innerHTML += value;
-        } else if (['+', '-', '*', '/'].includes(value)) {
-            let symbol;
-            if (value === '*') symbol = 'x';
-            else symbol = value;
-            result.innerHTML += symbol;
-            operator(value);
-            isOperator = true;
-        } else {
-            if (result.innerHTML === '0'){
-                result.innerHTML = value;
-                numTemp = value;
-                calcPartial = expression + numTemp;
-            } else {
-                result.innerHTML += value;
-                numTemp += value;
-                calcPartial = expression + numTemp;
-            }
-        }
+        isOperator = false;
+        hasComma = false;
+
+        return;
+    } 
+
+    if (isOperator) return;
+    
+    if (value === ',') {
+        if (hasComma) return;
+        if (result.innerHTML === '0') numTemp = '0.';
+        else numTemp += '.';
+        result.innerHTML += value;
+        hasComma = true;
+        return;
     }
+
+    let symbol;
+
+    if (value === '*') symbol = 'x';
+    else symbol = value;
+
+    result.innerHTML += symbol;
+    operator(value);
+    isOperator = true;
 }
 
 function operator(op) {
+    residue = eval(numTemp);
     expression += numTemp + op;
     numTemp = '';
-}
-
-function porcent () {
-    let por = numTemp/ 100;
-    if (expression === '') numTemp = por;
-    else numTemp = calcPartial * por;
 }
 
 function clear() {
     expression = '';
     calcPartial = '';
+    numTemp = '';
+    hasComma = false;
+    isOperator = false;
     result.innerHTML = '0';
     partial.innerHTML = '';
 }
 
 function eraser() {
-    result.innerHTML = result.innerHTML.slice(0, -1);
-    expression.slice(0, -1);
-    if (result.innerHTML.length < 2) {
-        clear();
+    if (result.innerHTML === '0') return;
+
+    if (isOperator) isOperator = false;
+
+    if (result.innerHTML.length < 2) clear();
+    else result.innerHTML = result.innerHTML.slice(0, -1);
+
+    if (numTemp === '') {
+        expression = expression.slice(0, -1);
+        calcPartial = expression + numTemp;
+        return;
+    } else numTemp = numTemp.slice(0, -1);  
+
+    if (numTemp === '') {
+        numTemp = expression.substring(expression.length - 1);
+        expression = expression.slice(0, -1);
+        calcPartial = expression;
+        return;
     }
+
+    calcPartial = expression + numTemp;
+}
+
+function porcent () {
+    let porcent;
+    if (expression === '') {
+        porcent = eval(numTemp) / 100;
+        clear();
+        show(porcent);
+    } else {
+        porcent = (residue * numTemp) / 100;
+        numTemp = '';
+        result.innerHTML = expression;
+        show(porcent);
+    };
 }
 
 function upDisplayParse() {
-    console.log(calcPartial);
+    calcPartial = calcPartial.replace(/\,/g, '.');
     partial.innerHTML = eval(calcPartial) ?? '';
 }
 
@@ -120,4 +168,15 @@ function calculate() {
     partial.innerHTML = '';
     calcPartial = '';
     expression = calculate;
+}
+
+function invertFunction() {
+    invertTrigger = !invertTrigger;
+
+    if (invertTrigger) deg.style.color = ''
+
+    trigger.forEach((item) => {
+        if (invertTrigger) item.style.display = 'inline-flex';
+        else item.style.display = 'none';
+    })
 }
